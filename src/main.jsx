36b487addabd1a59505bb137d66516c0879d1d61
MonnNster4 <monsantiago09@gmail.com>
2024-02-23 19:54:43 +0800

    update

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+//product provider
+import ProductProvider from "./contexts/ProductContext.jsx";
+//sidebar provider
+import SidebarProvider from "./contexts/SidebarContext.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <SidebarProvider>
+    <ProductProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ProductProvider>
+  </SidebarProvider>
+);
